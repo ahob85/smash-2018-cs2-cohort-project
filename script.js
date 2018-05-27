53bcdc4af192f4ede450d22073997210ef62e6b2
ahob85 <aaron@ahob85.com>
2018-05-27 03:02:40 -0700

    final commit

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 // Global Variables
 var p5Canvas;
 var myName;
+var speedSlider, redCheckbox, orangeCheckbox, blueCheckbox, greenCheckbox;
+var paused = false;
 
 // initialize global variables in setup() function
 function setup() {
-  p5Canvas = createCanvas(1000, 800);
+  p5Canvas = createCanvas(800, 600);
   p5Canvas.parent("#p5-canvas");
   myName = select("#my-name");
   myName.html("[Your Name Here]");
+  speedSlider = select("#speed-slider");
+  speedSlider.input(updateSpeed);
+  redCheckbox = select("#red-checkbox");
+  orangeCheckbox = select("#orange-checkbox");
+  blueCheckbox = select("#blue-checkbox");
+  greenCheckbox = select("#green-checkbox");
+}
 
 function draw() {
   background(100, 0, 100);
